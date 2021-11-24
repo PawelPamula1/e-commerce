a537a29ececffe4437d36dd5d85f606f00a433e7
PawelPamula1 <pawelpamula003@gmail.com>
2021-11-24 18:03:32 +0100

    cart logic

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -3,32 +3,7 @@ import { Grid } from '@material-ui/core';
 import Product from './Product/Product';
 import useStyles from './styles';
 
-const products = [
-  {
-    id: 1,
-    name: 'Shoes',
-    description: 'Running shoes.',
-    price: '$50',
-    image: 'https://assets.ajio.com/medias/sys_master/root/hc4/h09/13018715553822/-288Wx360H-460342492-blue-MODEL.jpg',
-  },
-  {
-    id: 2,
-    name: 'Macbook',
-    description: 'Apple Macbook.',
-    price: '$480',
-    image:
-      'https://www.vedion.pl/pol_pl_Laptop-Apple-Macbook-Air-A1465-i5-5-generacji-4GB-128GB-SSD-MJVM2LL-A-11-6-HD-Early-2015-Klasa-A-Certified-Refurbished-6382_1.jpg',
-  },
-  {
-    id: 3,
-    name: 'Umbrella',
-    description: 'Super Umbrella.',
-    price: '$5',
-    image: 'https://mi-store.pl/pol_pl_Parasol-Mi-Automatic-Umbrella-Black-338_1.jpg',
-  },
-];
-
-const Products = () => {
+const Products = ({ products, onAddToCart }) => {
   const classes = useStyles();
   return (
     <main className={classes.content}>
@@ -36,7 +11,7 @@ const Products = () => {
       <Grid container justify="center" spacing={4}>
         {products.map((product) => (
           <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
-            <Product product={product} />
+            <Product product={product} onAddToCart={onAddToCart} />
           </Grid>
         ))}
       </Grid>
